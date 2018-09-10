9941f8bbd8969e4d024915c26bc3e1c59538ac94
mastrogithub <mastrovitiseba@gmail.com>
2018-09-10 20:47:31 -0300

    Update of string connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mastrovitiseba:mastrolab123@ds139951.mlab.com:39951/mastrotodoapp');
+mongoose.connect('mongodb://todoappuser:user123@ds139951.mlab.com:39951/mastrotodoapp');
 //
 //'mongodb://localhost:27017/TodoApp'
 module.exports = { mongoose };
