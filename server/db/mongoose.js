3a182f40225d142a283b90373d572d63b8639f92
mastrogithub <mastrovitiseba@gmail.com>
2018-09-10 20:35:12 -0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+mongoose.connect('mongodb://mastrovitiseba:mastrolab123@ds139951.mlab.com:39951/mastrotodoapp');
+//
+//'mongodb://localhost:27017/TodoApp'
 module.exports = { mongoose };
